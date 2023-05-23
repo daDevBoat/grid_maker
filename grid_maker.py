df9cd4173cfbdf7f7f0f5cca955ec64cdb4d70c4
daDevBoat <113507675+daDevBoat@users.noreply.github.com>
2023-05-23 10:44:27 +0200

    Update grid_maker.py

--- grid_maker.py
+++ grid_maker.py
@@ -1,10 +1,13 @@
 """
 User instructions:
 
-Press 0-9 to select what number should be placed in the grid.
+Change the num_of_rows and num_of_cols variables to the desired number, and then run the program.
+
+The default grid layout consists of all zeroes (0). 
+Press 0-9 to select what number should be placed in the grid. 
 0 is blank, which basically makes it an eraser.
 
-Press enter or close the window when done drawing.
+Press enter or close the window when done drawing. 
 The grid will then be printed to the terminal.
 """
 import pygame as pg
